8029a1bd949deaf4367b3b31a9fa511494f4e28b
Mathias Huque <mh284934@fi365.ort.edu.uy>
2026-01-01 17:10:18 -0300

    Initialize React + TypeScript + Vite project
    Add initial project files including Vite config, TypeScript configs, ESLint setup, Tailwind CSS, and base React app structure. Includes .gitignore, README, and all necessary configuration and source files for a minimal React + TypeScript + Vite setup.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl text-blue-300 font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
