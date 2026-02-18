3562c2651e75ca50c66a38ba6d7fe1954a360506
Mathias Huque <mh284934@fi365.ort.edu.uy>
2026-02-18 11:40:01 -0300

    Update index.ts

--- components/home/index.ts
+++ components/home/index.ts
@@ -1,5 +1,4 @@
 export { default as HomeSection } from "./HomeSection";
 export { default as Hero } from "./Hero";
-export { default as Portrait } from "./Portrait";
 export { default as DragBadge } from "./DragBadge";
 export { default as TypingParagraph } from "./TypingParagraph";
